44e40aeec9e541f369e460721fcc3ff88b6dea29
alindaByamukama <alindabyamukama@gmail.com>
2023-07-24 23:02:29 +0300

    import/ require axios

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,13 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
+const axios = require('axios').default;
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-
+    // get or create user on chat engine
     try {
         const res = await axios.put(
             'https://api.chatengine.io/users',
